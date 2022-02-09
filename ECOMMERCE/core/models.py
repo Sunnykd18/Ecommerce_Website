68e30c966e983a49e7b1da45872bd75b33e25781
Sunnykd18 <95135405+Sunnykd18@users.noreply.github.com>
2022-02-09 21:31:46 +0530

    Adding items to cart

--- ECOMMERCE/core/models.py
+++ ECOMMERCE/core/models.py
@@ -19,10 +19,12 @@ LABEL_CHOICES = (
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
-    price = models.FloatField()
+    discount_price = models.FloatField(blank=True,null=True)
     category = models.CharField(choices=CATEGORY_CHOICES,max_length=2)
     label = models.CharField(choices=LABEL_CHOICES,max_length=1)
     slug = models.SlugField()
+    description = models.TextField()
+
     def __str__(self):
         return self.title
 
@@ -31,9 +33,15 @@ class Item(models.Model):
           'slug': self.slug
         })
 
+    def get_add_to_cart_url(self):
+        return reverse("core:add_to_cart" , kwargs={
+          'slug': self.slug
+        })
+
 
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
         return self.title
--- ECOMMERCE/core/urls.py
+++ ECOMMERCE/core/urls.py
@@ -3,7 +3,8 @@ from .views import (
     products,
     checkout,
      HomeView,
-     ItemDetailView
+     ItemDetailView,
+     add_to_cart
 )
 
 app_name = 'core'
@@ -11,5 +12,6 @@ app_name = 'core'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('checkout/', checkout, name='checkout'),
-    path('product/<slug>/', ItemDetailView.as_view(), name='product')
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_to_cart,name = 'add-to-cart')
 ]
